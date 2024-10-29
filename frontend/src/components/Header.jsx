b3f8069a872391be1e8ef6d0f754f0c1c7b7f2fb
Herik Trombetta Rodrigues <herikrodr@hotmail.com>
2024-10-29 16:37:16 -0300

    atualizaçao de codigo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,11 +4,16 @@ import React from 'react';
 function Header() {
   return (
     <header className='bg-backgroundPurple shadow-sm'>
-        <div className='justify-between max-w-5xl mx-auto px-4 py-4 flex items-center'>
-            <i className='text-white font-pixelfy text-3xl font-bold'>Herik<span className='text-spanHeader'>.dev</span></i>
-            <h1 className='text-white text-2xl font-bold'>Comparador Rainbow6</h1>
-            <h2 className='text-white text-1xl font-bold'>React + NodeJS + Tailwind</h2>
+      <div className='justify-between max-w-5xl mx-auto px-4 py-4 flex items-center'>
+        {/* Logo */}
+        <i className='text-white font-pixelfy text-3xl font-bold'>Herik<span className='text-spanHeader'>.dev</span></i>
+        
+        {/* Título e Subtítulo visíveis apenas em telas maiores */}
+        <div className='hidden md:flex flex-col items-start'>
+          <h1 className='text-white text-2xl font-bold'>Comparador Rainbow6</h1>
+          <h2 className='text-white text-xl font-bold'>React + NodeJS + Tailwind</h2>
         </div>
+      </div>
     </header>
   );
 }
